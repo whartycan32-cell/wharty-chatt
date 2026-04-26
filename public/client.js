2035a6f6f6bbf13bd7f616fda9822ffba6680120
whartycan32-cell <whartycan32@gmail.com>
2026-04-26 18:37:12 +0300

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,21 +1,37 @@
 const socket = io();
 
-const myName = localStorage.getItem("name");
+// otomatik isim (2 kişi için yeterli)
+let myName = "User_" + Math.floor(Math.random() * 1000);
 
-if(!myName){
-  window.location.href = "login.html";
-}
+// loading animasyonu (3 saniye)
+let progress = document.getElementById("progress");
+let width = 0;
 
-socket.emit("join", myName);
+let interval = setInterval(() => {
+  width += 1;
+  progress.style.width = width + "%";
 
+  if (width >= 100) {
+    clearInterval(interval);
+
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("app").classList.remove("hidden");
+
+    socket.emit("join", myName);
+  }
+}, 30);
+
+// eski mesajlar
 socket.on("loadMessages", (msgs) => {
   msgs.forEach(addMessage);
 });
 
+// yeni mesaj
 socket.on("newMessage", (msg) => {
   addMessage(msg);
 });
 
+// gönder
 function send(){
   const text = document.getElementById("msg").value;
   if(!text) return;
@@ -24,6 +40,7 @@ function send(){
   document.getElementById("msg").value = "";
 }
 
+// mesaj ekleme
 function addMessage(msg){
   const div = document.createElement("div");
   div.classList.add("msg");
