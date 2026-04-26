b0cea43386645baa6597db1e46be7c8d371dae7d
whartycan32-cell <whartycan32@gmail.com>
2026-04-26 21:41:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,32 +9,24 @@ const io = new Server(server);
 app.use(express.static("public"));
 
 let users = {};
-let data = {};
-let online = {};
+let messages = [];
 
 io.on("connection", (socket) => {
 
-  // REGISTER
+  // KAYIT
   socket.on("register", ({ username, password }) => {
     if (!username || !password)
       return socket.emit("registerError", "Boş bırakma");
 
     if (users[username])
-      return socket.emit("registerError", "Zaten var");
+      return socket.emit("registerError", "Bu kullanıcı var");
 
     users[username] = password;
-
-    data[username] = {
-      friends: [],
-      requests: [],
-      messages: {},
-      avatar: ""
-    };
 
     socket.emit("registerSuccess");
   });
 
-  // LOGIN
+  // GİRİŞ
   socket.on("login", ({ username, password }) => {
     if (!users[username])
       return socket.emit("loginError", "Kullanıcı yok");
@@ -43,104 +35,25 @@ io.on("connection", (socket) => {
       return socket.emit("loginError", "Şifre yanlış");
 
     socket.username = username;
-    online[username] = true;
 
-    io.emit("onlineList", Object.keys(online));
-
-    socket.emit("loginSuccess", {
-      username,
-      friends: data[username].friends,
-      requests: data[username].requests,
-      avatar: data[username].avatar
-    });
+    socket.emit("loginSuccess", { username });
+    socket.emit("loadMessages", messages);
   });
 
-  // DISCONNECT
-  socket.on("disconnect", () => {
-    if (socket.username) {
-      delete online[socket.username];
-      io.emit("onlineList", Object.keys(online));
-    }
-  });
-
-  // AVATAR
-  socket.on("setAvatar", (url) => {
-    data[socket.username].avatar = url;
-  });
-
-  // SEARCH
-  socket.on("searchUser", (q) => {
-    const result = Object.keys(users).filter(u =>
-      u.toLowerCase().includes(q.toLowerCase())
-    );
-    socket.emit("searchResult", result);
-  });
-
-  // FRIEND REQUEST
-  socket.on("sendRequest", (to) => {
-    const from = socket.username;
-
-    if (!data[to]) return;
-
-    if (!data[to].requests.includes(from)) {
-      data[to].requests.push(from);
-    }
-
-    socket.emit("notify", "İstek gönderildi ✔");
-  });
-
-  // ACCEPT
-  socket.on("acceptRequest", (from) => {
-    const user = socket.username;
-
-    data[user].friends.push(from);
-    data[from].friends.push(user);
-
-    data[user].requests =
-      data[user].requests.filter(u => u !== from);
-
-    socket.emit("updateFriends", data[user].friends);
-  });
-
-  // DM
-  socket.on("sendDM", ({ to, text }) => {
-    const from = socket.username;
-    const key = [from, to].sort().join("-");
-
-    if (!data[from].messages[key]) data[from].messages[key] = [];
-    if (!data[to].messages[key]) data[to].messages[key] = [];
+  // MESAJ
+  socket.on("sendMessage", (text) => {
+    if (!socket.username) return;
 
     const msg = {
-      from,
-      text,
-      seen: false,
-      time: new Date().toLocaleTimeString()
+      user: socket.username,
+      text
     };
 
-    data[from].messages[key].push(msg);
-    data[to].messages[key].push(msg);
+    messages.push(msg);
 
-    io.emit("newDM", { key, msg });
-  });
-
-  socket.on("getDM", (u) => {
-    const key = [socket.username, u].sort().join("-");
-    socket.emit("loadDM", data[socket.username].messages[key] || []);
-  });
-
-  socket.on("seen", (u) => {
-    const key = [socket.username, u].sort().join("-");
-    const msgs = data[socket.username].messages[key] || [];
-
-    msgs.forEach(m => m.seen = true);
-
-    io.emit("seenUpdate", key);
-  });
-
-  socket.on("typing", (to) => {
-    io.emit("typing", { from: socket.username, to });
+    io.emit("newMessage", msg);
   });
 
 });
 
-server.listen(3000, () => console.log("Çalışıyor..."));
+server.listen(3000, () => console.log("Çalışıyor"));
